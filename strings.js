6e95eed60847fce9ce92e593cbd4ff49b705bd2b
LHEIK444 <kiehlharvey11@gmail.com>
2024-09-15 22:12:51 +0800

    Create strings.js

---
+++ strings.js
@@ -0,0 +1,17 @@
+let favActorFirstName = "Kristen";
+let favActorLastName = "Stewart";
+let fullName = favActorFirstName + " " + favActorLastName;
+
+let uppercase = fullName.toLowerCase();
+let lowercase = "Luke".toUpperCase();
+
+let message = "My favorite actor is" + " "+ fullName. toLocaleUpperCase();
+
+message += ", his best show is twilight.";
+
+console.log(favActorFirstName);
+console.log(favActorLastName);
+console.log(fullName);
+console.log(uppercase);
+console.log(lowercase);
+console.log(message);
